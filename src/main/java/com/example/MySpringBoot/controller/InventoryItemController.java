cce7a580c0c5f9d3eb4aa581d30b86075f0e3a3f
Dor Shamir <dorshamir4@gmail.com>
2021-03-29 15:42:52 +0300

    Done

--- src/main/java/com/example/MySpringBoot/controller/InventoryItemController.java
+++ src/main/java/com/example/MySpringBoot/controller/InventoryItemController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 //@RequestMapping("/")
 public class InventoryItemController {
 
